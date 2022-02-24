73b4e75b6c4b8400039a2096928f971f198da576
Justin Luciano <justinlluciano@gmail.com>
2022-02-24 07:24:01 -0800

    commit tree 100cc8a70e707de2672742c380fa3b93f7ebcb4f

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,18 @@
 class Post < ApplicationRecord
+  validates :title, presence: true
+  validates :content, length: { minimum: 100 }
+  validate :select_categories
+
+  private
+
+  CATEGORIES = [
+    /Fiction/i,
+    /Non-Fiction/i
+  ]
+
+  def select_categories
+    if CATEGORIES.none? { |cat| cat.match category }
+      errors.add(:category, "is not included in the list")
+    end
+  end
 end
